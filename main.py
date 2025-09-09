20ba841fedb38855e6ad9c244119483e0bff3fe6
nanachichi <116679354+nanachichi@users.noreply.github.com>
2025-09-09 21:19:12 +0300

    Add get_num_of_words() function

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+
+def get_num_of_words(file_contents):
+    words = file_contents.split()
+    num_of_words = len(words)
+    return num_of_words
