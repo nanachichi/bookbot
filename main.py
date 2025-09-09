bd6539c131822db8dbec51f6cecdc49de394bebe
nanachichi <116679354+nanachichi@users.noreply.github.com>
2025-09-09 22:43:01 +0300

    Update main() function

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from stats import get_num_of_words, times_char_appears
+from stats import get_num_of_words, times_char_appears, sort_times_char_appears
+
+
+filepath = "books/frankenstein.txt"
 
 
 def get_book_text(filepath):
@@ -8,11 +11,21 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text(filepath)
     word_count = get_num_of_words(book_text)
     char_count = times_char_appears(book_text)
-    print(word_count)
-    print(char_count)
+    list_char_appears = sort_times_char_appears(char_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for dictionary in list_char_appears:
+        if dictionary["char"].isalpha():
+            count = f'{dictionary["char"]}: {dictionary["num"]}'
+            print(count)
+    print("============= END ===============")
 
 
 main()
