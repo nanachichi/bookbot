b9855682515864829304bd5aa6d5dbd5c198179a
nanachichi <116679354+nanachichi@users.noreply.github.com>
2025-09-09 21:44:48 +0300

    Add times_char_appears() function)

--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def get_num_of_words(file_contents):
     words = file_contents.split()
     num_of_words = len(words)
     return num_of_words
+
+
+def times_char_appears(book_text):
+    chars = list(book_text)
+    times_char_appears = {}
+    for char in chars:
+        char = char.lower()
+        if char in times_char_appears:
+            times_char_appears[char] += 1
+        else:
+            times_char_appears[char] = 1
+    return times_char_appears
