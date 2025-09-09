09b512a04f6bf7c45fd0e10ae7a68c823d147e8a
nanachichi <116679354+nanachichi@users.noreply.github.com>
2025-09-09 22:56:36 +0300

    Make a soft coded book path in main()
    By removing the hard coded book path and using the built in sys module

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from stats import get_num_of_words, times_char_appears, sort_times_char_appears
-
-
-filepath = "books/frankenstein.txt"
+import sys
 
 
 def get_book_text(filepath):
@@ -11,21 +9,27 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text(filepath)
-    word_count = get_num_of_words(book_text)
-    char_count = times_char_appears(book_text)
-    list_char_appears = sort_times_char_appears(char_count)
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
 
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {filepath}...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count} total words")
-    print("--------- Character Count -------")
-    for dictionary in list_char_appears:
-        if dictionary["char"].isalpha():
-            count = f'{dictionary["char"]}: {dictionary["num"]}'
-            print(count)
-    print("============= END ===============")
+        book_text = get_book_text(filepath)
+        word_count = get_num_of_words(book_text)
+        char_count = times_char_appears(book_text)
+        list_char_appears = sort_times_char_appears(char_count)
+
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {filepath}...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count} total words")
+        print("--------- Character Count -------")
+        for dictionary in list_char_appears:
+            if dictionary["char"].isalpha():
+                count = f'{dictionary["char"]}: {dictionary["num"]}'
+                print(count)
+        print("============= END ===============")
 
 
 main()
