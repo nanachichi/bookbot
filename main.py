4d9d76c803c8c85d9736a987fd8347fe8fbe4593
nanachichi <116679354+nanachichi@users.noreply.github.com>
2025-09-09 21:45:42 +0300

    Update main.py
    Import the new function and add main()

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from stats import get_num_of_words
+from stats import get_num_of_words, times_char_appears
 
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
+
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    word_count = get_num_of_words(book_text)
+    char_count = times_char_appears(book_text)
+    print(word_count)
+    print(char_count)
+
+
+main()
